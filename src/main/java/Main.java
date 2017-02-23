45022983b9f244e28db393591fefb73f7ba29f7c
dempey <empey.david@gmail.com>
2017-02-23 13:51:21 -0700

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/pioneer", (req, res) -> "Welcome to Pioneer");
 
     get("/", (request, response) -> {
         Map<String, Object> attributes = new HashMap<>();
